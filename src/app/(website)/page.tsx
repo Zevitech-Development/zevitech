f4784c5c6369f5414a1b9241e47093b1ebf024da
Zevitech Development <zevitechdevelopment@gmail.com>
2026-06-25 20:54:22 +0100

    made changes

--- src/app/(website)/page.tsx
+++ src/app/(website)/page.tsx
@@ -3,7 +3,6 @@ import { Metadata } from "next";
 import HomePage from "@/containers/website/home-page";
 
 import { GetPageMetadata } from "@/utils/meta-data";
-
 export const metadata: Metadata = GetPageMetadata({
   title: "Zevitech | Software, Apps & Digital Transformation",
   description:
