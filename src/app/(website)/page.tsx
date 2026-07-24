b19e1a664006ecd626d7cd2684bbea333dec6c2a
Zevitech Development <zevitechdevelopment@gmail.com>
2026-07-25 00:23:46 +0500

    made changes

--- src/app/(website)/page.tsx
+++ src/app/(website)/page.tsx
@@ -8,7 +8,6 @@ export const metadata: Metadata = GetPageMetadata({
   description:
     "Zevitech is a full-spectrum digital solutions agency offering mobile apps, web platforms, SaaS, UI/UX design, eCommerce, SEO, automation, and branding.",
 });
-
 export default function Home() {
   return (
     <main>
